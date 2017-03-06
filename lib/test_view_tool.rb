bbeef929ce51aaccb31dd174e994ea870b16a4b8
docoder <docoder@163.com>
2017-03-06 11:21:43 +0800

    init

---
+++ lib/test_view_tool.rb
@@ -0,0 +1,6 @@
+require "test_view_tool/version"
+require "test_view_tool/render"
+
+module TestViewTool
+  # Your code goes here...
+end
---
+++ lib/test_view_tool/version.rb
@@ -0,0 +1,3 @@
+module TestViewTool
+  VERSION = "0.1.0"
+end
